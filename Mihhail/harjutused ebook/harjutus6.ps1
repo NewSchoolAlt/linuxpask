ad0c8d9d1648c550b532e95c241a9167ce899914
olev savi <olev.savi@voco.ee>
2025-02-10 11:49:16 +0200

    e

--- Mihhail/harjutused ebook/harjutus6.ps1
+++ Mihhail/harjutused ebook/harjutus6.ps1
@@ -1,5 +1,6 @@
 $csv = Import-Csv C:\temp\students.csv -Header Name, Age
 
+
 $csv | ForEach-Object {
     if ($_.'Age' -lt 18) {
         $_ | Add-Member -NotePropertyName 'Status' -NotePropertyValue 'Junior' -Force
